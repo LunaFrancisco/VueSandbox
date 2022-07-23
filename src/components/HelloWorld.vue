1eb2b53726be0a5de73a08c2b38a3fe8c35b7f82
LunaFrancisco <francisco.luna@mail.udp.cl>
2022-07-23 03:15:23 -0400

    added helloworld

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template lang="">
+  <div>
+    <h1>Hello, world!</h1>
+    <p>This is a template for a simple Vue.js app.</p>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="">
+  
+</style>
