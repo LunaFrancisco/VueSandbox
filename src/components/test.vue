619a2c19911497c6edb0207815037f8ba2863148
LunaFrancisco <francisco.luna@mail.udp.cl>
2022-07-23 04:45:45 -0400

    fakecorrido front using naive-ui

---
+++ src/components/test.vue
@@ -0,0 +1,43 @@
+<script>
+import { defineComponent } from 'vue'
+import { NConfigProvider, NInput, NDatePicker, NSpace } from 'naive-ui'
+// theme
+import { createTheme, inputDark, datePickerDark } from 'naive-ui'
+
+
+export default defineComponent({
+    components: {
+        NConfigProvider,
+        NInput,
+        NDatePicker,
+        NSpace
+    },
+    setup() {
+        return {
+            darkTheme: createTheme([inputDark, datePickerDark]),
+
+        }
+    }
+})
+</script>
+
+<template>
+    <NConfigProvider theme={this.darkTheme}>
+       
+            <h1>Hello, world!</h1> 
+            <p>This is a template for a simple Vue.js app.</p>
+        
+        <n-config-provider :theme="darkTheme" :locale="zhCN" :date-locale="dateZhCN">
+            <n-space vertical>
+                <n-input />
+                <n-date-picker />
+            </n-space>
+        </n-config-provider>
+    </NConfigProvider>
+</template>
+
+<style>
+body {
+    background: black;
+}
+</style>
